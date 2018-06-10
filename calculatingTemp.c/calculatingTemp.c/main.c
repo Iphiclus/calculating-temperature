95cb591748738d972de6dd29209b93cfc2a094b2
Georgina Hall <Georgina.Hall@HMCTS.NET>
2018-06-10 21:23:26 +0100

    First commit.

---
+++ calculatingTemp.c/calculatingTemp.c/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	float celsius = 0, fahrenheit, kelvin;
+
+	while (celsius < 100)
+	{
+		celsius = celsius + 10;
+		fahrenheit = celsius * 9 / 5 + 32;
+		kelvin = celsius + 273.15f;
+
+		printf("%.2f C = %.2f F = %.2f K\n",
+			celsius,
+			fahrenheit,
+			kelvin);
+	}
+	printf("Press ENTER key to Continue\n");
+	getchar();
+}
